b6d029691f088863d306c0cebb403a162adf3355
Cassandra Gallagher <ccgall@gmail.com>
2016-04-04 21:01:55 -0700

    update wordpress theme

--- wp-content/themes/stanleywp-child/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Add any new theme functions
-
-//Import Parent Styles
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 99 );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
