26db467e287e18391383d4d6ddc8f375f0cd0514
Cassandra Gallagher <ccgall@gmail.com>
2016-04-04 19:51:00 -0700

    update functions to call style.css

--- wp-content/themes/stanleywp-child/functions.php
+++ wp-content/themes/stanleywp-child/functions.php
@@ -1,2 +1,8 @@
 <?php
 //Add any new theme functions
+
+//Import Parent Styles
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 99 );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
